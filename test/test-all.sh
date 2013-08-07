3b04f3af492c99450c5afcc598046ac665ba6001
David Z. Chen <d.zhekai.chen@gmail.com>
2013-08-07 14:05:07 -0700

    Remove output test files. Add test-codegen.py

--- test/test-all.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python test-all.py
---
+++ test/test-codegen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+from subprocess import *
+
+TEST_DIRECTORY = 'test/codegen'
+
+def main():
+  for _, _, files in os.walk(TEST_DIRECTORY):
+    for file in files:
+      if not (file.endswith('.decaf')):
+        continue
+      ref_name = os.path.join(TEST_DIRECTORY, "%s.out" % file.split('.')[0])
+      test_name = os.path.join(TEST_DIRECTORY, file)
+      result = Popen('./dcc < ' + test_name + ' > tmp.asm', 
+                     shell = True, stderr = STDOUT, stdout = PIPE)
+      result = Popen('diff - ' + ref_name, 
+                     shell = True, stdin = result.stdout, stdout = PIPE)
+      print 'Executing test "%s"' % test_name
+      print ''.join(result.stdout.readlines())
+
+if __name__ == '__main__':
+  main()
+
+# vim: set ai ts=2 sts=2 sw=2 et::q
+
